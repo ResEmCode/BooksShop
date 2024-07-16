ed2d346a58eeebba1c5d02eae3d2a68620386c15
Meow-Double <slava.pugachev.2015@mail.ru>
2024-07-16 19:24:47 +0300

    fix styles in ProfilePage

--- src/pages/Profile/ui/Profile.tsx
+++ src/pages/Profile/ui/Profile.tsx
@@ -10,22 +10,22 @@ const Profile = () => {
     <div className={s.container}>
       <Header />
       <div className={s.user}>
-        <div className={s.user_block}>
+        <div className={s.userBlock}>
           <img
             className={s.avatar}
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSptPGSNaOiSoh1QUmzzaVts8auSzZ3hXUPjw&s'
             alt=''
           />
-          <div className={s.user_info}>
+          <div className={s.userInfo}>
             <h2 className={s.name}>Nickname</h2>
             <p>Пол: Мужской</p>
             <p>Возраст: 1488</p>
             <p>Любимые жанры: хоррор, детектив, мистика, боевики, бла-бла-бла</p>
           </div>
         </div>
-        <div className={s.user_about}>
-          <h4 className={s.user_about_text}>About me</h4>
-          <div className={s.user_text}>
+        <div className={s.userAbout}>
+          <h4 className={s.userAboutText}>About me</h4>
+          <div className={s.userText}>
             LormeSDafghfgddddddddddddddddddddddddddddddddddfdscgdvhbnm,bnbvncgfdfghnhmnb
           </div>
         </div>
